43d4bc7f7604fab25b4f606ec8fba6f4366bc72c
safadtm <safadtm2004@gmail.com>
2023-12-07 17:51:28 +0530

    Video Card --created

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,55 +2,57 @@ import { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
 
 import { fetchFromApi } from '../utils/fetchFromAPI'
-import {Sidebar,Videos} from './'
+import { Sidebar, Videos } from './'
 
 
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState('New')
-
-  useEffect(()=>{
-  fetchFromApi(`search?part=snippet&q=${selectedCategory}`);
-  },[selectedCategory]);
+  const [videos, setVideos] = useState([])
+  
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items));
+  }, [selectedCategory]);
 
   return (
     <Stack
       sx={{ flexDirection: { sx: "column", md: "row" } }}
     >
-       <Box
-       sx={{height:{sx:'auto',md:'92vh'},borderRight:'1px solid #3d3d3d',px:{sx:0,md:2}}}
-       >
+      <Box
+        sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}
+      >
 
-       <Sidebar
-       selectedCategory={selectedCategory}
-       setSelectedCategory={setSelectedCategory}
-       />   
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
 
-       <Typography
-       className='copyright'
-       variant='body2'
-       sx={{mt:1.5,color:'#fff'}}
-       >
-        Copyright 2023 Safad T M
-       </Typography>
-       </Box>
+        <Typography
+          className='copyright'
+          variant='body2'
+          sx={{ mt: 1.5, color: '#fff' }}
+        >
+          Copyright 2023 Safad T M
+        </Typography>
+      </Box>
 
-       <Box p={2} sx={{
-        overflowY:'auto',
-        height:'90vh',
-        flex:2
-       }}>
+      <Box p={2} sx={{
+        overflowY: 'auto',
+        height: '90vh',
+        flex: 2
+      }}>
 
         <Typography variant='h4'
-        fontWeight="bold"
-        mb={2}
-        sx={{
-          color:'white'
-        }}>
-         {selectedCategory} <span style={{color:'#F31503'}}>videos</span>
+          fontWeight="bold"
+          mb={2}
+          sx={{
+            color: 'white'
+          }}>
+          {selectedCategory} <span style={{ color: '#F31503' }}>videos</span>
         </Typography>
 
-        <Videos videos={[]}/>
-       </Box>
+        <Videos videos={videos} />
+      </Box>
     </Stack>
   )
 }
